c12b142ec0f6a7045d15932178a0b1a032965f7a
ivileey <nishuiaee@126.com>
2020-05-13 16:43:11 +0800

    ClassLoder

---
+++ DeepunderstandingJVM/src/chapter2/TestClass.java
@@ -0,0 +1,11 @@
+package chapter2;
+
+/**
+ * 测试类
+ */
+public class TestClass {
+	public static void main(String[] args) {
+		System.out.println("-----this is test class out println----");
+		System.out.println("hahhahahaha");
+	}
+}
